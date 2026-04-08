b865bf1fab0148482dbaca9ea297aa5c672244f6
Yannick Lang <layaxx@gmx.de>
2026-04-08 09:12:00 +0200

    chore: update Docker files

--- api/config/plugins.ts
+++ api/config/plugins.ts
@@ -1,5 +1,12 @@
 import type { Core } from "@strapi/strapi"
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({})
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  "config-sync": {
+    enabled: true,
+    config: {
+      importOnBootstrap: true,
+    },
+  },
+})
 
 export default config
